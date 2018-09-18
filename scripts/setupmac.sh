fcb2d8aee08ee04bc841a2854e90b38d8cfdf62c
Marais Rossouw <me@maraisr.com>
2018-09-18 13:37:51 +1000

    chore: setupmac to also run link

--- scripts/setupmac.sh
+++ scripts/setupmac.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Start with brew things
+brew update
+brew upgrade
+
+brew install vim --override-system-vi
+brew install openssl jq yq fish watch
+
+sudo pip install boto boto3 jinja2
+
 # Mac specific things
 defaults write com.apple.universalaccess reduceTransparency -bool true
 defaults write com.apple.dock mru-spaces -bool true && \
@@ -12,12 +21,5 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-brew update
-brew upgrade
-
-brew install vim --override-system-vi
-brew install openssl jq yq fish watch
-
-sudo pip install boto boto3 jinja2
-
 ./nix.sh # Runs my *nix things
+./link.sh # Link all my config files into this home folder
