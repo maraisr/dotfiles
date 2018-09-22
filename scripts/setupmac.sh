e34131272dac0fd9ede715797cbdf7d834311f63
Marais Rossouw <hi@marais.io>
2018-09-22 11:30:38 +1000

    chore: added more mac things

--- scripts/setupmac.sh
+++ scripts/setupmac.sh
@@ -11,17 +11,25 @@ sudo pip install boto boto3 jinja2
 
 # Mac specific things
 defaults write com.apple.universalaccess reduceTransparency -bool true
+
 defaults write com.apple.dock mru-spaces -bool true && \
 	killall Dock
 defaults write com.apple.dock no-bouncing -bool true && \
 	killall Dock
+
+# Finder things
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true && \
 	defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
+
+# Keybase repeat rates
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
-./nix.sh # Runs my *nix things
-./link.sh # Link all my config files into this home folder
+# Don't save .DS_Store files to Network Shares
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+
+./scripts/nix.sh # Runs my *nix things
+./scripts/link.sh # Link all my config files into this home folder
