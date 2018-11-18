cbbfdc9bea92d6d167537b60b224adfecb447a54
Marais Rossouw <me@maraisr.com>
2018-11-19 08:02:04 +1000

    chore: key remap for home/end keys

--- scripts/setupmac.sh
+++ scripts/setupmac.sh
@@ -23,13 +23,29 @@ defaults write -g NSNavPanelExpandedStateForSaveMode -bool true && \
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Keybase repeat rates
+# Key repeat rates
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
 # Don't save .DS_Store files to Network Shares
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+
+# Home/End button remapped to begin/end of lines
+mkdir -p ~/Library/KeyBindings/
+if [[ ! -e /Scripts/file.txt ]]; then
+	touch ~/Library/KeyBindings/DefaultKeyBinding.dict
+	cat <<EOT >> ~/Library/KeyBindings/DefaultKeyBinding.dict
+{
+    "\UF729"  = moveToBeginningOfLine:; // home
+    "\UF72B"  = moveToEndOfLine:; // end
+    "$\UF729" = moveToBeginningOfLineAndModifySelection:; // shift-home
+    "$\UF72B" = moveToEndOfLineAndModifySelection:; // shift-end
+}
+EOT
+else
+		echo "Error KeyBindings file already exists. Please check"
+fi
 
 ./scripts/nix.sh # Runs my *nix things
 ./scripts/link.sh # Link all my config files into this home folder
