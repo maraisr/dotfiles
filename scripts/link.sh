850c12f0536eee3292bdf473d214c5f6fd5dae60
Marais Rossouw <me@maraisr.com>
2019-08-04 14:07:15 +1000

    chore: Added more git things + volta install

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,3 +2,6 @@
 
 ln -s $(pwd)/tmux/.tmux.conf ~
 ln -s $(pwd)/vim/.vimrc ~
+
+cp -f $(pwd)/git/.gitconfig ~
+cp -f $(pwd)/git/.gitignore-global ~
--- scripts/nix.sh
+++ scripts/nix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install Volta
+curl https://get.volta.sh | bash
+
 # Get Plug (Vim Plugin Manager)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
