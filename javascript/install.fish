2c31f5e469a064e356fdaccf69467294022ebb99
Marais Rossouw <me@marais.co>
2022-08-04 20:26:56 +1000

    feat: back to volta :rocket:

--- javascript/install.fish
+++ javascript/install.fish
@@ -1,15 +1,6 @@
 #!/usr/bin/env fish
 
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+curl -fsSL https://get.volta.sh | bash -s
 
-fnm install 18
-fnm default 18
-
-mkdir -p $HOME/.corepack
-
-corepack enable --install-directory $HOME/.corepack
-set -Ua fish_user_paths $HOME/.corepack
-
-if not command -qs pnpm
-    corepack prepare pnpm@7.6.0 --activate
-end
+volta install node@latest
+volta install pnpm@latest
