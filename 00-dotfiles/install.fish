b2587c6c75d38bacf1e87b40a97cb813ae16ed8b
Marais Rossouw <me@marais.co>
2023-03-09 10:00:19 +1000

    chore: small delights

--- 00-dotfiles/install.fish
+++ 00-dotfiles/install.fish
@@ -1,18 +1,31 @@
 #!/usr/bin/env fish
 
+# =========
+# Variables
+# =========
+
 set -Ux EDITOR vim
 set -Ux VISUAL $EDITOR
 
 set -Ux PROJECTS ~/dev
 
-mkdir -p $PROJECTS
+# =========
+# Functions
+# =========
 
 for f in $DOTFILES/*/functions
 	set -Up fish_function_path $f
 end
+
+# =======
+# Configs
+# =======
 
 mkdir -p ~/.config/fish/conf.d/
 
 for f in $DOTFILES/*/conf.d/*.fish
 	ln -sf $f ~/.config/fish/conf.d/(basename $f)
 end
+
+
+mkdir -p $PROJECTS
