57cd7d59b0925b61816dc9cfd7e1a97be6d74a46
Marais Rossouw <me@marais.co>
2024-05-11 11:51:59 +1000

    fix: maybe this fixes vim PlugUpdate?

--- vim/install.fish
+++ vim/install.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -Es +'PlugUpdate | PlugInstall --sync' +qa
+
+vim +'PlugUpdate | PlugInstall' +qall
