6cf7193d351a1f625f8eacd22a456d568c8dee8b
Marais Rossouw <me@marais.co>
2024-12-24 13:48:59 +1000

    chore: we actually want pinentry-mac

--- gnupg/install.fish
+++ gnupg/install.fish
@@ -7,8 +7,7 @@ end
 mkdir -p ~/.gnupg
 test -f ~/.gnupg/gpg-agent.conf || touch ~/.gnupg/gpg-agent.conf
 
-# set pinentry-tty
-grep -q "pinentry-program" ~/.gnupg/gpg-agent.conf || echo "pinentry-program "(which pinentry-tty) >> ~/.gnupg/gpg-agent.conf
+grep -q "pinentry-program" ~/.gnupg/gpg-agent.conf || echo "pinentry-program "(which pinentry-mac) >> ~/.gnupg/gpg-agent.conf
 
 # ask password every 24hours only
 grep -q "default-cache-ttl" ~/.gnupg/gpg-agent.conf || echo "default-cache-ttl 86400" >> ~/.gnupg/gpg-agent.conf
