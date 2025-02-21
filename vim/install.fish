aa108e6adb972cb078bdae682d356bee6e314576
Marais Rossouw <me@marais.co>
2025-02-21 10:07:58 +1000

    chore: remove vim-plug from Darwin right now

--- vim/install.fish
+++ vim/install.fish
@@ -1,5 +1,10 @@
 #!/usr/bin/env fish
 
+# TODO: Vim plug install, just hangs in a codespace? Is there a no tty option?
+if test (uname) != Darwin
+    exit
+end
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
